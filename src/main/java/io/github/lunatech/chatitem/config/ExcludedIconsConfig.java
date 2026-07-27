460cebdd829f01901b8312792b0054e28cc75b3c
lunarenzo <luna.renzo.zamora@gmail.com>
2026-07-27 23:35:07 +0800

    fix: Implement VersionedConfig interface in ExcludedIconsConfig to fix compiler generic constraint error

--- src/main/java/io/github/lunatech/chatitem/config/ExcludedIconsConfig.java
+++ src/main/java/io/github/lunatech/chatitem/config/ExcludedIconsConfig.java
@@ -1,12 +1,22 @@
 package io.github.lunatech.chatitem.config;
 
+import org.spongepowered.configurate.interfaces.meta.Exclude;
 import org.spongepowered.configurate.objectmapping.ConfigSerializable;
 import org.spongepowered.configurate.objectmapping.meta.Comment;
 
 import java.util.List;
 
 @ConfigSerializable
-public class ExcludedIconsConfig {
+public class ExcludedIconsConfig implements VersionedConfig {
+    @Comment("Do not change this value!")
+    public int configVersion = 1;
+
+    @Override
+    @Exclude
+    public int configVersion() {
+        return configVersion;
+    }
+
     @Comment("List of item IDs (material names) or wildcard patterns whose icons should not be shown in chat.\n" +
              "You can specify exact material names (e.g. MACE, HEAVY_CORE) or wildcard suffixes (e.g. *_FENCE, *_STAIRS).")
     public List<String> excludedIcons = List.of(
